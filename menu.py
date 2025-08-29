3d7801a4d27982903d66b613dbd1eb90e867715a
Jeffrey Johnston <rpgplayers.inc@gmail.com>
2025-08-30 02:25:28 +1000

    Refactor menu structure to use a submenu system for tests, consolidating options into categories. Introduce a new submenu.py file for handling compression, telnet, display, and encoding tests, enhancing organization and maintainability.

--- menu.py
+++ menu.py
@@ -1,55 +1,26 @@
+from submenu import MAIN_CATEGORIES
 
-from testdata import ANSI_TEST, UTF8_TEST, MCCP2_TEST
-from mudsocket import send, slowsend
-
-from nawstest import nawstest_handler
-
-from optionscan import optionscan_handler
-from emptysubneg import emptysubneg_handler
-from echo import echo_handler
-from ttype import ttype_handler
-
-from baudtest import baudtest_handler
-from xterm256 import xterm256_handler
-from truecolor import truecolor_handler
-
-OPTIONS = {
-    "ansi": send(ANSI_TEST),
-    "ansi_slow": slowsend(ANSI_TEST),
-    "utf": send(UTF8_TEST),
-    "utf_slow": slowsend(UTF8_TEST),
-    "mccp2": send(MCCP2_TEST, newline_replace=False),
-    "mccp2_slow": slowsend(MCCP2_TEST, newline_replace=False),
-    "naws": nawstest_handler,
-    "optionscan": optionscan_handler,
-    "emptysubneg": emptysubneg_handler,
-    "echo": echo_handler,
-    "ttype": ttype_handler,
-    "baudtest": baudtest_handler,
-    "xterm256": xterm256_handler,
-    "truecolor": truecolor_handler,
-}
-
-# Create word-wrapped options list
 def create_menu():
-    # Options with word wrapping at 80 chars
-    options_text = "Options are: " + ", ".join(OPTIONS)
-    wrapped_options = ""
-    current_line = ""
+    menu_text = "\r\nMain Menu\r\n=========\r\n"
+    menu_text += "Categories (Select to see more details about each test):\r\n"
+    for key, (desc, _) in MAIN_CATEGORIES.items():
+        menu_text += f"  {key:<15} - {desc}\r\n"
     
-    for word in options_text.split():
-        if len(current_line + word + " ") <= 80:
-            current_line += word + " "
-        else:
-            wrapped_options += current_line.rstrip() + "\r\n"
-            current_line = word + " "
-    wrapped_options += current_line.rstrip() + "\r\n"
+    # Show all direct options
+    menu_text += "\r\nDirect access (or use category above):\r\n"
+    menu_text += "  Compression:    mccp2, mccp2_slow, mccp4, mccp4_deflate\r\n"
+    menu_text += "  Telnet:         naws, optionscan, emptysubneg, echo, ttype\r\n" 
+    menu_text += "  Display:        ansi, ansi_slow, xterm256, truecolor\r\n"
+    menu_text += "  Encoding:       utf, utf_slow\r\n"
+    menu_text += "  Comprehensive:  baudtest\r\n"
     
-    return (wrapped_options + "\r\n" + 
-            "Try 'baudtest' for a comprehensive capability test!\r\n").encode()
+    menu_text += "\r\nChoose a category or specific test: "
+    return menu_text.encode()
 
 HELLO = (
     "Welcome to The Mud Client Test Server\r\n"
     "How would you like to torture your mud client?\r\n").encode()
 
 MENU = create_menu()
+
+OPTIONS = MAIN_CATEGORIES
